0087c756a157f0e9ccbee3ec02a21d0c813e40a9
Jordy van Vorselen <jordyvanvorselen@gmail.com>
2020-07-10 21:07:26 +0200

    Use HTTPS always

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,8 @@ use Mix.Config
 config :leaderboards, LeaderboardsWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: false
+  check_origin: false,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
